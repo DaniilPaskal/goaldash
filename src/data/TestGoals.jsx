1dc2a52e56521e10000d70a903cc583787d0fed5
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-14 23:12:54 -0400

    Created goal test data

---
+++ src/data/TestGoals.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+const TestGoals = [
+    {
+        id: 1,
+        name: 'Get a job',
+        progress: 35,
+        taskQueues: [
+            {
+                name: 'Study English',
+                tasks: [
+                    {
+                        name: 'Duolingo',
+                    }
+                ]
+            },
+            {
+                name: 'Study JavaScript',
+                tasks: [
+
+                ],
+            },
+        ],
+    },
+];
+
+export default TestGoals;
