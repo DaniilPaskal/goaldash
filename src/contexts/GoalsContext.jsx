2c2eeaf7ff7e07ebe992a11e4e44fa69a54c9e22
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-14 23:13:11 -0400

    Loaded test data

--- src/contexts/GoalsContext.jsx
+++ src/contexts/GoalsContext.jsx
@@ -1,10 +1,15 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
+import TestGoals from "../data/TestGoals";
 
 const GoalsContext = React.createContext();
 
 export function GoalsProvider({}) {
     const [goals, setGoals] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setGoals(TestGoals);
+    })
 
     return (
         <GoalsProvider.Provider value={goals}>
