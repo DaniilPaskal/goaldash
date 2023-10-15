15252b96fe07ad40838b68b56b86ab22855f14e5
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-14 23:14:08 -0400

    Created task progress indicator component

---
+++ src/components/TaskIndicator.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+const TaskIndicator = ({ status }) => {
+    const colors = {
+        ahead: 'rgb(83, 216, 50)',
+        onTrack: 'rgb(55, 178, 226)',
+        behind: 'rgb(226, 186, 55)',
+    };
+    const color = colors[status];
+
+    return (
+        <div className='task-indicator' style={{ color: color }} />
+    );
+}
+
+export default TaskIndicator;
