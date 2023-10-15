0cd2ada12c2f5d2e76a7751b7cd401bd580b035d
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-14 23:16:29 -0400

    Started work on dashboard task panel

---
+++ src/components/TaskPanel.jsx
@@ -0,0 +1,25 @@
+import TaskIndicator from './TaskIndicator';
+import './App.css';
+
+const TaskPanel = ({ task }) => {
+    const { name } = task;
+
+    return (
+        <div className='task-panel'>
+            <TaskIndicator progress='' />
+            <h2>{name}</h2>
+            
+            <button>
+                Finish
+            </button>
+            <button>
+                Postpone
+            </button>
+            <button>
+                Edit
+            </button>
+        </div>
+    );
+}
+
+export default TaskPanel;
