bca9fe748a40ed97c1248047e6988921426937cc
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-15 23:59:54 -0400

    Worked on task editor

--- src/components/TaskEditor.jsx
+++ src/components/TaskEditor.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { Task } from './Classes';
 import './App.css';
 
-const TaskEditor = () => {
+const emptyTask = {
+    name: '',
+    locked: false
+}
 
+const TaskEditor = ({ task = emptyTask }) => {
+    const [newTask, setNewTask] = useState(task);
+    const {} = newTask;
+
+    return (
+        <form className='task-form'>
+            <label for='name'>Name:</label>
+            <input type='text' id='name' name='name' required />
+
+            <input type='submit' value='Save' />
+        </form>
+    );
 }
 
 export default TaskEditor;
