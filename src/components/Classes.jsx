293519f9fd084794752e4bfc316d627c489d1226
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-15 23:54:49 -0400

    Created classes for goals, task lists, and tasks

---
+++ src/components/Classes.jsx
@@ -0,0 +1,80 @@
+export class Task {
+    constructor(name, duration) {
+        this.name = name;
+        this.duration = {
+            total: duration,
+            complete: 0
+        };
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+export class TaskList {
+    constructor(name) {
+        this.name = name;
+        this.head = null;
+    }
+
+    insert(index, task) {
+        if (index === 0) {
+            if (this.head) {
+                task.next = this.head;
+                this.head.prev = task;
+            }
+            this.head = task;
+            return;
+        }
+
+        var prevTask = this.head;
+
+        for (var i = 0; i < index; i++) {
+            prevTask = prevTask.next;
+        }
+
+        var nextTask = prevTask.next;
+
+        task.next = nextTask;
+        task.prev = prevTask;
+        prevTask.next = task;
+        nextTask.prev = task;
+    }
+
+    remove(index) {
+        if (index === 0) {
+            var newHead = this.next.head;
+            newHead.prev = null;
+            this.head = newHead;
+            return;
+        }
+
+        var prevTask = this.head;
+
+        for (var i = 0; i < index; i++) {
+            prevTask = prevTask.next;
+        }
+
+        var removeTask = prevTask.next;
+        var nextTask = removeTask.next;
+
+        prevTask.next = nextTask;
+        nextTask.prev = prevTask;  
+    }
+}
+
+export class Goal {
+    constructor(name, endDate) {
+        this.name = name;
+        this.endDate = endDate;
+        this.taskLists = [];
+    }
+
+    addTaskList(taskList) {
+        this.taskLists.push(taskList);
+    }
+
+    removeTaskList(taskList) {
+        var index = this.TaskLists.findIndex(taskList);
+        taskList.splice(index, 1);
+    }
+}
