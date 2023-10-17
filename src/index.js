ca2ee6ad8017342443e63a2eb772e31f0feebae7
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-16 23:59:34 -0400

    Imported Bootstrap CSS

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./hooks/ScrollToTop";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faPencil, faUser } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop from "./hooks/ScrollToTop";
 
 library.add(faBars, faPencil, faUser);
 
