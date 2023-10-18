354e6bd5ca85a2a527d9ab692be9daa4fdcbe605
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-18 06:32:37 -0400

    Worked on task management page

--- src/pages/TaskManagement.jsx
+++ src/pages/TaskManagement.jsx
@@ -1,7 +1,24 @@
-import './App.css';
+import { useState } from 'react';
+import { useGoals } from '../contexts/GoalsContext';
+import GoalEditor from '../components/GoalEditor';
+import GoalPanel from '../components/GoalPanel';
 
 const TaskManagement = () => {
-    
+    const [show, setShow] = useState(false);
+    const goals = useGoals();
+
+    return (
+        <div>
+            <button onClick={() => setShow(true)}>Create goal</button>
+            <GoalEditor show={show} setShow={setShow} />
+
+            {goals.map((goal) => {
+                return (
+                    <GoalPanel goal={goal} />
+                );
+            })}
+        </div>
+    );
 }
 
 export default TaskManagement;
