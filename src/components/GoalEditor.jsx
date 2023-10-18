cc211db40288fb9183c6d557b82d7995d883a945
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-18 06:32:17 -0400

    Created goal editor form

--- src/components/GoalEditor.jsx
+++ src/components/GoalEditor.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import { Modal } from "bootstrap";
+import { saveGoal } from "./DataFunctions";
 
-const GoalEditor = ({ show, setShow }) => {
+const emptyGoal = {
+    name: '',
+    endDate: ''
+};
+
+const GoalEditor = ({ show, setShow, goal = emptyGoal }) => {
+    const [newGoal, setNewGoal] = useState(goal);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNewGoal({ ...newGoal, [name]: value });
+    }
+
+    const handleSave = () => {
+        saveGoal(newGoal);
+    }
+
+    const handleDelete = () => {
+
+    }
+
     return (
         <Modal show={show} onHide={() => setShow(false)} size='lg'>
-            <label for='name'>Name:</label>
-            <input type='text' id='name' name='name' required />
+            <Modal.Header closeButton />
+            <Modal.Body>
+                <label for='name'>Name:</label>
+                <input type='text' id='name' name='name' defaultValue={newGoal.name} onChange={handleChange} required />
 
-            
+                <label for='endDate'>End date:</label>
+                <input type='date' id='endDate' name='endDate' defaultValue={newGoal.endDate} onChange={handleChange} required />
+
+                <button onClick={handleSave}></button>
+            </Modal.Body>
         </Modal>
     );
 }
