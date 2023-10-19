1aa94df93b5733b27bbda8e75bbb0a29508c038c
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-18 23:02:32 -0400

    Wrapped app in goals provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import GoalsProvider from './contexts/GoalsContext';
 import LandingPage from "./pages/LandingPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route index element={<LandingPage />} />
-      </Routes>
+      <GoalsProvider>
+        <Routes>
+          <Route index element={<LandingPage />} />
+        </Routes>
+      </GoalsProvider>
     </div>
   );
 }
