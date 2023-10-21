f1edcee6138be55542662f796e9eee24881db765
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-20 23:32:38 -0400

    Temporarily removed modal

--- src/components/GoalEditor.jsx
+++ src/components/GoalEditor.jsx
@@ -24,18 +24,15 @@ const GoalEditor = ({ show, setShow, goal = emptyGoal }) => {
     }
 
     return (
-        <Modal show={show} onHide={() => setShow(false)} size='lg'>
-            <Modal.Header closeButton />
-            <Modal.Body>
-                <label for='name'>Name:</label>
-                <input type='text' id='name' name='name' defaultValue={newGoal.name} onChange={handleChange} required />
+        <div>
+            <label for='name'>Name:</label>
+            <input type='text' id='name' name='name' defaultValue={newGoal.name} onChange={handleChange} required />
 
-                <label for='endDate'>End date:</label>
-                <input type='date' id='endDate' name='endDate' defaultValue={newGoal.endDate} onChange={handleChange} required />
+            <label for='endDate'>End date:</label>
+            <input type='date' id='endDate' name='endDate' defaultValue={newGoal.endDate} onChange={handleChange} required />
 
-                <button onClick={handleSave}></button>
-            </Modal.Body>
-        </Modal>
+            <button onClick={handleSave}>Save</button>
+        </div>
     );
 }
 
