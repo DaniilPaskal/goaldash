18f1cd43a857d0761bd691fafd54e1143faa1296
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-22 20:21:43 -0400

    Added task editor button to task list panel

--- src/components/TaskListPanel.jsx
+++ src/components/TaskListPanel.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import TaskListEditor from "./TaskListEditor";
+import TaskEditor from "./TaskEditor";
 import ProgressBar from "./ProgressBar";
 import '../App.css';
 
 const TaskListPanel = ({ TaskList }) => {
     const [showTaskListEditor, setShowTaskListEditor] = useState(false);
+    const [showTaskEditor, setShowTaskEditor] = useState(false);
     const { name, head } = TaskList;
+
 
     return (
         <div className='tasklist-panel'>
             <h1>{name}</h1>
 
             <button onClick={() => setShowTaskListEditor(true)}>Edit TaskList</button>
-            <TaskListEditor show={showTaskListEditor} setShow={setShowTaskListEditor} TaskList={TaskList} />
+            <TaskListEditor show={showTaskListEditor} setShow={setShowTaskListEditor} taskList={TaskList} />
 
-            
+            <button onClick={() => setShowTaskEditor(true)}>Add task</button>
+            <TaskEditor show={showTaskEditor} setShow={setShowTaskEditor} />
 
             
         </div>
