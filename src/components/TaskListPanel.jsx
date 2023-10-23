b767291a9530b67662598f64a1b08f9b2cb03a07
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-22 20:30:51 -0400

    Added list of tasks to task list panel

--- src/components/TaskListPanel.jsx
+++ src/components/TaskListPanel.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TaskListEditor from "./TaskListEditor";
 import TaskEditor from "./TaskEditor";
+import TaskPanel from "./TaskPanel";
 import ProgressBar from "./ProgressBar";
 import '../App.css';
 
@@ -8,7 +9,13 @@ const TaskListPanel = ({ TaskList }) => {
     const [showTaskListEditor, setShowTaskListEditor] = useState(false);
     const [showTaskEditor, setShowTaskEditor] = useState(false);
     const { name, head } = TaskList;
+    const taskNode = head;
+    const taskArray = [];
 
+    while (taskNode) {
+        taskArray.push(taskNode);
+        taskNode = taskNode.next;
+    }
 
     return (
         <div className='tasklist-panel'>
@@ -20,6 +27,9 @@ const TaskListPanel = ({ TaskList }) => {
             <button onClick={() => setShowTaskEditor(true)}>Add task</button>
             <TaskEditor show={showTaskEditor} setShow={setShowTaskEditor} />
 
+            {taskArray.map((task) => {
+                <TaskPanel task={task} />
+            })}
             
         </div>
     );
