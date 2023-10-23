c0b2369a7a2164b7c2094350c8e386536112ab7e
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-23 05:48:16 -0400

    Fixed issues with adding and replacing goals

--- src/components/DataFunctions.jsx
+++ src/components/DataFunctions.jsx
@@ -2,9 +2,10 @@ import { Goal, TaskList, Task } from "./Classes";
 
 export function saveGoal(goals, goalData) {
     const newGoal = new Goal(goalData.name, goalData.endDate);
-    const index = goals.map((goal) => goal.id).indexOf(newGoal.id);
+    const index = goals.map((goal) => goal.id).indexOf(goalData.id);
 
     if (index > -1) {
+        newGoal.setId(goalData.id);
         goals[index] = newGoal;
     } else {
         newGoal.setId(goals.length);
