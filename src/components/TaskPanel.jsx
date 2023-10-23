9d266780992b60addb90d186588e0e288776407e
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-10-23 05:57:41 -0400

    Changed panel to fit task management page

--- src/components/TaskPanel.jsx
+++ src/components/TaskPanel.jsx
@@ -1,22 +1,17 @@
+import { useState } from 'react';
+import TaskEditor from './TaskEditor';
 import TaskIndicator from './TaskIndicator';
 
 const TaskPanel = ({ task }) => {
+    const [showTaskEditor, setShowTaskEditor] = useState(false);
     const { name } = task;
 
     return (
         <div className='task-panel'>
-            <TaskIndicator progress='' />
             <h2>{name}</h2>
-            
-            <button>
-                Finish
-            </button>
-            <button>
-                Postpone
-            </button>
-            <button>
-                Edit
-            </button>
+
+            <button onClick={() => setShowTaskEditor(true)}>Edit task</button>
+            <TaskEditor show={showTaskEditor} setShow={setShowTaskEditor} task={task} />
         </div>
     );
 }
