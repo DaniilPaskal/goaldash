3edb14739d9688cd9ae977fb26d3ad6a5958e6c7
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-11-02 21:49:54 -0400

    Work on daily task display

--- src/components/DailyTaskDisplay.jsx
+++ src/components/DailyTaskDisplay.jsx
@@ -1,9 +1,18 @@
+import TaskIndicator from './TaskIndicator';
+
 const DailyTaskDisplay = ({ dailyTask }) => {
-    const { name, duration } = dailyTask;
+    const { name, duration, status } = dailyTask;
 
     return (
         <div>
-
+            <h3>{name}</h3>
+            <p>{duration}</p>
+            <div>
+                <TaskIndicator status={status} />
+                <button>Done</button>
+                <button>Postpone</button>
+                <button>Edit</button>
+            </div>
         </div>
     );
 }
